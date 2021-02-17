1a6cd7904371d2bc5fdd41c223aa3e7617b1d9d5
Nicolás Calvache <nicolas.calvache@alumnos.uneatlantico.es>
2021-02-17 20:26:47 +0100

    removed unused functions

--- src/scripts/lib.js
+++ src/scripts/lib.js
@@ -1,28 +1,6 @@
-/**
- * Function to
- */
 function clearForm() {
   document.getElementById("username").value = "";
   document.getElementById("password").value = "";
-}
-
-/**
- * Creates a Hash for strings
- * @param {String} string
- * @returns Hash for the string
- */
-function stringToHash(string) {
-  let hash = 0;
-
-  if (string.length == 0) return hash;
-
-  for (i = 0; i < string.length; i++) {
-    char = string.charCodeAt(i);
-    hash = (hash << 5) - hash + char;
-    hash = hash & hash;
-  }
-
-  return hash;
 }
 
 function crypt(string) {
